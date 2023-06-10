ce44d95d15e87987d6c5668604afeea27038816b
SergeyParadigm77 <paradigm77@rambler.ru>
2023-06-10 21:47:24 +0300

    Курсовая работа номер 2 выполнена!

--- src/main/java/pro/sky/coursework2/service/ExaminerService.java
+++ src/main/java/pro/sky/coursework2/service/ExaminerService.java
@@ -1,5 +1,5 @@
 package pro.sky.coursework2.service;
-import pro.sky.coursework2.repository.Question;
+import pro.sky.coursework2.model.Question;
 import java.util.Collection;
 
 public interface ExaminerService {
--- src/main/java/pro/sky/coursework2/service/impl/ExaminerServiceImpl.java
+++ src/main/java/pro/sky/coursework2/service/impl/ExaminerServiceImpl.java
@@ -1,11 +1,13 @@
 package pro.sky.coursework2.service.impl;
 
 import org.springframework.stereotype.Service;
-import pro.sky.coursework2.repository.Question;
+import pro.sky.coursework2.exceptions.TooManyQuestionsException;
+import pro.sky.coursework2.model.Question;
 import pro.sky.coursework2.service.ExaminerService;
 import pro.sky.coursework2.service.QuestionService;
 
 import java.util.Collection;
+import java.util.HashSet;
 
 @Service
 public class ExaminerServiceImpl implements ExaminerService {
@@ -17,6 +19,13 @@ public class ExaminerServiceImpl implements ExaminerService {
 
     @Override
     public Collection<Question> getQuestions(int amount) {
-        return null;
+        if (questionService.getAll().size() < amount) {
+            throw new TooManyQuestionsException();
+        }
+        final Collection<Question> questions = new HashSet<>();
+        while (questions.size() != amount) {
+           questions.add(questionService.getRandomQuestion());
+        }
+        return questions;
     }
 }
